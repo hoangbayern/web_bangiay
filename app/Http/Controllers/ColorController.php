1e4c1452bd96afbfe0e5cd32c109c66b866a59c2
hoangbayern <hoangdm2002@gmail.com>
2023-11-07 18:24:35 +0700

    feature color product admin

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -2,9 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Color\StoreRequest;
+use App\Http\Requests\Color\UpdateRequest;
+use App\Models\Color;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ColorController extends Controller
 {
-    //
+    protected Color $color;
+
+    /**
+     * @param Color $color
+     */
+    public function __construct(Color $color)
+    {
+        $this->color = $color;
+    }
+
+    public function listColor()
+    {
+        return view('admin.color.list');
+    }
+
+    public function search(Request $request)
+    {
+        $colors = $this->color->search($request->name);
+        $view = view('admin.color.table', compact('colors'))->render();
+        return response()->json([
+            'table' => $view,
+        ], Response::HTTP_OK);
+    }
+
+    public function showFormCreate()
+    {
+        return view('admin.color.create');
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $colorData = $this->color->create($request->all());
+        return response()->json([
+            'data' => $colorData,
+            'message' => 'Color Added Successfully.'
+        ], Response::HTTP_CREATED);
+    }
+
+    public function showFormEdit(string $id)
+    {
+        $color = $this->color->findOrFail($id);
+        return view('admin.color.edit', compact('color'));
+    }
+
+    public function update(UpdateRequest $request, string $id)
+    {
+        $color = $this->color->findOrFail($id);
+        $color->update($request->all());
+        return redirect()->route('color.list')->withErrors([
+            'success' => 'Color Updated Successfully.'
+        ]);
+    }
+
+    public function deleteColor(string $id)
+    {
+        $colorId = $this->color->findOrFail($id);
+        $colorId->delete();
+        return response()->json([
+            'message' => 'Color Deleted Successfully.'
+        ], Response::HTTP_OK);
+    }
 }
---
+++ resources/views/admin/color/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.main')
+@section('contents')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Edit Color</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{route('adminHome')}}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{route('color.list')}}">List Size</a></li>
+                            <li class="breadcrumb-item active">Edit Color</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">General</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{route('color.update', $color->id)}}" method="post" name="categoryForm">
+                                @csrf
+                                @method('POST')
+                                <div class="form-group required">
+                                    <label for="inputName">Color Name</label>
+                                    <input type="text" id="input-name" name="name" value="{{$color->name}}" class="form-control">
+                                    <span id="name-error" class="error invalid-feedback"></span>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="inputStatus">Status</label>
+                                    <select id="inputStatus" name="status" class="form-control custom-select">
+                                        <option {{($color->status === 1) ? 'selected' : ''}} value="1">Active</option>
+                                        <option {{($color->status === 0) ? 'selected' : ''}} value="0">Inactive</option>
+                                    </select>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-12">
+                                        <a href="{{route('color.list')}}" class="btn btn-secondary">Cancel</a>
+                                        <button type="submit" class="btn btn-success float-right">Update</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
