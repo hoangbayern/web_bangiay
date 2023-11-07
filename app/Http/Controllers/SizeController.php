b755c46e8fec70e597e0082bbe23f9490161bf4c
hoangbayern <hoangdm2002@gmail.com>
2023-11-07 11:32:40 +0700

    feature size product admin

--- app/Http/Controllers/SizeController.php
+++ app/Http/Controllers/SizeController.php
@@ -2,9 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Size\StoreRequest;
+use App\Http\Requests\Size\UpdateRequest;
+use App\Models\Size;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SizeController extends Controller
 {
-    //
+    protected Size $size;
+
+    /**
+     * @param Size $size
+     */
+    public function __construct(Size $size)
+    {
+        $this->size = $size;
+    }
+
+    public function listSize()
+    {
+        return view('admin.size.list');
+    }
+
+    public function search(Request $request)
+    {
+        $sizes = $this->size->search($request->name);
+        $view = view('admin.size.table', compact('sizes'))->render();
+        return response()->json([
+            'table' => $view,
+        ], Response::HTTP_OK);
+    }
+
+    public function showFormCreate()
+    {
+        return view('admin.size.create');
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $sizeData = $this->size->create($request->all());
+        return response()->json([
+            'data' => $sizeData,
+            'message' => 'Size Added Successfully.',
+        ], Response::HTTP_CREATED);
+    }
+
+    public function showFormEdit(string $id)
+    {
+        $size = $this->size->findOrFail($id);
+        return view('admin.size.edit', compact('size'));
+    }
+
+    public function update(UpdateRequest $request, string $id)
+    {
+        $size = $this->size->findOrFail($id);
+        $size->update($request->all());
+        return redirect()->route('size.list')->withErrors([
+            'success' => 'Size Updated Successfully.'
+        ]);
+    }
+
+    public function deleteSize(string $id)
+    {
+        $sizeId = $this->size->findOrFail($id);
+        $sizeId->delete();
+        return response()->json([
+            'message' => 'Size Deleted Successfully.',
+        ], Response::HTTP_OK);
+    }
 }
---
+++ resources/views/admin/size/create.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.main')
+@section('contents')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Create Size</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{route('adminHome')}}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{route('size.list')}}">List Size</a></li>
+                            <li class="breadcrumb-item active">Create Size</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">General</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{route('size.store')}}" method="post" id="form-data" name="categoryForm">
+                                @csrf
+                                @method('POST')
+                                <div class="form-group required">
+                                    <label for="inputName">Size Name</label>
+                                    <input type="text" id="input-name" name="name" class="form-control">
+                                    <span id="name-error" class="error invalid-feedback"></span>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="inputStatus">Status</label>
+                                    <select id="inputStatus" name="status" class="form-control custom-select">
+                                        <option value="1">Active</option>
+                                        <option value="0">Inactive</option>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-12">
+                    <a href="{{route('size.list')}}" class="btn btn-secondary">Cancel</a>
+                    <button type="button" class="btn btn-success float-right btn-save">Create</button>
+                </div>
+            </div>
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
